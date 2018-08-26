7cf81fea7395e188cbba08fc3d75a91b274766dd
EniMarlinah <enimarlinah20@gmail.com>
2018-08-26 18:31:06 +0700

    v-01

---
+++ app/src/main/java/com/example/user/uas/Server.java
@@ -0,0 +1,8 @@
+package com.example.user.uas;
+/**
+ * Created by Angga Hendrawan on 13/08/2018.
+ */
+
+public class Server {
+    public static final String URL = "http://sasmitoh.nitarahmawati.my.id/";
+}
